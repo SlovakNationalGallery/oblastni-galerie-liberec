f0703b1a7b6fb5a0b77849e2a566e38e51987982
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-02-11 10:53:34 +0100

    add i18n

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
@@ -10,6 +11,28 @@ const router = createRouter({
     }]
 })
 
+const messages = {
+    cs: {}
+}
+
+const i18n = createI18n({
+    locale: 'cs',
+    fallbackLocale: 'cs',
+    pluralizationRules: {
+        cs: function (choice) {
+            if (choice === 1) {
+                return 0
+            } else if (choice > 1 && choice < 5) {
+                return 1
+            } else {
+                return 2
+            }
+        }
+    },
+    messages,
+})
+
 createApp(App)
     .use(router)
+    .use(i18n)
     .mount('#app')
